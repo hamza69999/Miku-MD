a4ade6e41062a659990f58e5e94c2b04f3d2c9eb
hamza69999 <115677209+hamza69999@users.noreply.github.com>
2022-12-11 10:28:04 +0500

    Update config.js

--- config.js
+++ config.js
@@ -7,15 +7,15 @@ global.autoReadAll = false
 global.antitags = true
 
 
-global.Owner = ['918101187835']
-global.OwnerNumber = ['918101187835']
-global.ownertag = ['918101187835']
-global.BotName = "Miku"
-global.packname = "Miku bot"
-global.author = "By: Fantox"
-global.OwnerName = "Fantox"
+global.Owner = ['923425164467']
+global.OwnerNumber = ['923425164467']
+global.ownertag = ['923425164467']
+global.BotName = "Hamza"
+global.packname = "Sarar"
+global.author = "By: "Hamza"
+global.OwnerName = "Sardar Hamza"
 global.BotSourceCode = "https://github.com/FantoX001/Miku-MD"
-global.SupportGroupLink = "https://chat.whatsapp.com/JcBzjRIxM5UHfuJbt0KrWt"
+global.SupportGroupLink = "https://wa.me/message/75RBLRIWCG2UK1"
 global.sessionName = "session"
 
 
@@ -23,7 +23,7 @@ global.sessionName = "session"
 global.prefa = ['-'] // Prefix ---  change it to anythong you want except @//
 
 
-global.location = "West Bengal, India"
+global.location = "Pakistan"
 global.reactmoji = "❤️"
 global.themeemoji = "💖"
 global.vidmenu = { url: 'https://c.tenor.com/YGuLegQWubwAAAPo/miku-nakano-gotoubun-no-hanayome.mp4' }
